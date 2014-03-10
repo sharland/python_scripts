d356d30e49c06542a49cd19cc4d185eb0afec969
sharland <sharland@gmail.com>
2014-03-10 15:10:56 +0000

    basic lights script

---
+++ pibrella/basic_lights/lights_ON.py
@@ -0,0 +1,14 @@
+import pibrella
+import time
+
+print("Red light is on!")
+pibrella.light.red.on()
+time.sleep(2)
+
+print("Amber light is on!")
+pibrella.light.amber.on()
+time.sleep(2)
+
+print("Green light is on!")
+pibrella.light.green.on()
+time.sleep(2)
