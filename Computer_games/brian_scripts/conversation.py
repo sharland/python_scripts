5720fa7518308b303d55791d498fdd8a87c01209
sharland <sharland@gmail.com>
2014-03-30 17:54:31 +0000

    no idea what i'm uploading :-)

---
+++ Computer_games/brian_scripts/conversation.py
@@ -0,0 +1,8 @@
+firstName = input("What is your first name?")
+myAge = int(input("How old are you?")
+if firstName == "Brian":
+	print("Hello " + firstName)
+	myAge = myAge+1
+	print("Next year you will be ",myAge)
+else:
+	print("I don't know you ...")
